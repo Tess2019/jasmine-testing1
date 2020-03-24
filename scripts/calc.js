66ce770902d1e7198fb25031391e86381da0468e
Tess2019 <terese_burman@hotmail.com>
2020-03-24 07:39:09 +0000

    breaking test

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,6 +1,11 @@
-function addition(numOne, numTwo) {
-    if(typeof(numOne) == "number" && typeof(numTwo) == "number") {
-        return numOne + numTwo; 
+Calculator = function(){
+    this.value =0;
+}
+
+
+Calculator.prototype.sdd =function(number) {
+    if(typeof(number)=="number"){
+        this.value += number;
     }else{
         alert ("Error!");
     } 
