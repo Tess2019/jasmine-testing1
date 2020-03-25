bd4da8f2b329be0e004fa167efdd9e564eda6dd6
Tess2019 <terese_burman@hotmail.com>
2020-03-25 17:13:04 +0000

    fixing the functions in drinkSpec

--- spec/drinkSpec.js
+++ spec/drinkSpec.js
@@ -8,33 +8,33 @@ describe ("checks age", function(){
     expect (whatCanIDrink).toBeDefined();
 });
 
-it ("should reurn drink toddy when called as whatCanIDrink(< 13) ", funktion() {
+it ("should reurn drink toddy when called as whatCanIDrink(13) ", funktion() {
 var result =  WhatCanIDrink(13)
 expect (result).toBe ("Drink toddy");
 });
 
 it ("should return drink coke when called as whatCanIDrink(17) ", funktion(){
-    var result =WhatCanIDrink (17)
-expect (result). toBe ("Drink coke");
+    var result =whatCanIDrink (17)
+expect (result). toBe ("Drink coke")
 });
 
 it ("should return drink beer when called as whatCanIDrink(20)", funktion(){
-    var result = WhatCanIDrink (20)
-expect (result). toBe ("Drink beer");
+    var result = whatCanIDrink (20)
+expect (result).toBe ("Drink beer");
 });
 
 it ("should return drink whisky when called as whatCanIDrink(30)", funktion(){
-    var result =WhatCanIDrink (30)
+    var result =whatCanIDrink (30)
 expect(result).toBe ("Drink whisky");
 });
 
 it ("should return drink when called as whatCanIDrink(140)", funktion(){
-    var result =WhatCanIDrink (140)
+    var result =whatCanIDrink (140)
 expect (result).toBe ("Sorry. I can’t tell what drink because that age is incorrect!");
 });
 
 it ("should return drink when test whatCanIDrink()called ", function(){
-    var result = WhatCanIDrink().indexOf ("drink");
+    var result = whatCanIDrink().indexOf ("drink");
     expect (result).not.toEqual (-1);
 } );
 });
